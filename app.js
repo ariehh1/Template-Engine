7188542b83a538164f529ebe8ca6186b7b809d10
Ari Horowitz <ariehh@comcast.net>
2020-02-06 14:37:54 -0800

    returning json files, updating library files with correct paths, template literal output

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 "use strict";
 
 const inquirer = require("inquirer");
-const Employee = require();
-const Engineer = require();
-const Intern = require();
-const Manager = require();
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const generateMain = require("./templates/generatemain");
+console.log(generateMain("Ari", "engineer"));
+
+//initator for app
+
+//entry point
+
+//start with questions
+
+//classes to populate items
--- test/employee.test.js
+++ test/employee.test.js
@@ -2,46 +2,48 @@
 
 //what goes in require call-back?
 // how to obtain package.json and package-lock.json
-const Employee = require();
+const Employee = require("../lib/employee");
 
-test("can set name", () => {
-  const name = "Bill";
-  const employee = new Employee(name);
-  expect(employee.name).toBe(name);
-});
+describe("Employee", () => {
+  test("can set name", () => {
+    const name = "Bill";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+  });
 
-test("can set id value", () => {
-  const testValue = 350;
-  const employee = new Employee("", testValue);
-  expect(employee.id).toBe(testValue);
-});
+  test("can set id value", () => {
+    const testValue = 350;
+    const employee = new Employee("", testValue);
+    expect(employee.id).toBe(testValue);
+  });
 
-test("can set email alias", () => {
-  const testEmail = "test@test.com";
-  const employee = new Employee("", testEmail);
-  expect(employee.email).toBe(testEmail);
-});
+  test("can set email alias", () => {
+    const testEmail = "test@test.com";
+    const employee = new Employee("", 8, testEmail);
+    expect(employee.email).toBe(testEmail);
+  });
 
-test("can get name of getName()", () => {
-  const testValue = "Stacey";
-  const employee = new Employee(testValue);
-  expect(employee.getName()).toBe(testValue);
-});
+  test("can get name of getName()", () => {
+    const testValue = "Stacey";
+    const employee = new Employee(testValue);
+    expect(employee.getName()).toBe(testValue);
+  });
 
-test("can get id of getID()", () => {
-  const testValue = 675;
-  const employee = new Employee("", testValue);
-  expect(employee.getID()).toBe(testValue);
-});
+  test("can get id of getID()", () => {
+    const testValue = 675;
+    const employee = new Employee("", testValue);
+    expect(employee.getID()).toBe(testValue);
+  });
 
-test("can get email of getEmail()", () => {
-  const testEmail = "test@test.com";
-  const employee = new Employee("", testEmail);
-  expect(employee.getEmail()).toBe(testEmail);
-});
+  test("can get email of getEmail()", () => {
+    const testEmail = "test@test.com";
+    const employee = new Employee("", 8, testEmail);
+    expect(employee.getEmail()).toBe(testEmail);
+  });
 
-test("getRole() must return 'Employee'", () => {
-  const testValue = "Employee";
-  const employee = new Employee("Bill", 1, "test@test.com");
-  expect(employee.getRole()).toBe(testValue);
+  test("getRole() must return 'Employee'", () => {
+    const testValue = "Employee";
+    const employee = new Employee("Bill", 1, "test@test.com");
+    expect(employee.getRole()).toBe(testValue);
+  });
 });
